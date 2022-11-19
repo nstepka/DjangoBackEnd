1d447798d9ec7deac1543d98e4577f79b6ed324c
Nicholas Stepka <40475549+nstepka@users.noreply.github.com>
2022-11-18 20:42:11 -0600

    Update serializers.py

--- ABCAPP/serializers.py
+++ ABCAPP/serializers.py
@@ -29,31 +29,31 @@ class Abc_clientSerializer(serializers.ModelSerializer):
 class Storage_typeSerializer(serializers.ModelSerializer):
     class Meta:
         model = StorageType
-        field = ('storage_type_id', 'type_name', 'created_by', 'created_date', 'modified_by', 'modified_date', 'is_deleted')
+        fields = ('storage_type_id', 'type_name', 'created_by', 'created_date', 'modified_by', 'modified_date', 'is_deleted')
 
 
 class Inventory_typeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Inventory
-        field = ('inventory_id', 'abc_client_id', 'inventory_name', 'storage_type_id', 'max_item_capacity', 'address', 'created_by', 'created_date',
+        fields = ('inventory_id', 'abc_client_id', 'inventory_name', 'storage_type_id', 'max_item_capacity', 'address', 'created_by', 'created_date',
                  'modified_by', 'modified_date', 'is_deleted')
 
 
 class Resource_typeSerializer(serializers.ModelSerializer):
     class Meta:
         model = ResourceType
-        field = ('resource_type_id', 'type_name', 'created_by',
+        fields = ('resource_type_id', 'type_name', 'created_by',
                  'created_date', 'modified_by', 'modified_date', 'is_deleted')
 
 
 class Abc_resourceSerializer(serializers.ModelSerializer):
     class Meta:
         model = AbcResource
-        field = ('abc_resource_id', 'inventory_id', 'resource_type_id', 'resource_name', 'max_number_of_resources', 'current_number_of_resources', 'created_by', 'created_date',
+        fields = ('abc_resource_id', 'inventory_id', 'resource_type_id', 'resource_name', 'max_number_of_resources', 'current_number_of_resources', 'created_by', 'created_date',
                  'modified_by', 'modified_date', 'is_deleted')
 
 
 class access_logSerializer(serializers.ModelSerializer):
     class Meta:
         model = AccessLog
-        field = ('access_log_id', 'username', 'resource_type_id', 'action', 'table_name', 'field_name', 'screen_name', 'created_date')
+        fields = ('access_log_id', 'username', 'resource_type_id', 'action', 'table_name', 'field_name', 'screen_name', 'created_date')
